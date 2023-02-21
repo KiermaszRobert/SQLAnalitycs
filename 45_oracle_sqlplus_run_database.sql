1fb3ebe7ccc9766124bbae5e42c0c510c6b3041f
KiermaszRobert <125459636+KiermaszRobert@users.noreply.github.com>
2023-02-21 10:34:55 +0100

    Add files via upload

---
+++ 45_oracle_sqlplus_run_database.sql
@@ -0,0 +1,19 @@
+srvctl status database -db mail
+
+sqlplus / as sysdba
+
+@?/demo/schema/human_resources/hr_main.sql
+
+
+
+
+alter session set "_ORACLE_SCRIPT"=true;
+
+create user hr identified by hr;
+
+drop user hr cascade;
+
+@?/demo/schema/human_resources/hr_main.sql
+
+
+SET ORACLE_SID=orcl3
